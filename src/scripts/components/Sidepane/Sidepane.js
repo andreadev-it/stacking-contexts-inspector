f3a6a40ff5ca66867ade591d0f067df56270a31f
Andrea <dev@andreadev.it>
2021-07-26 17:51:33 +0200

    Added warning inside panel sidepane

--- src/scripts/components/Sidepane/Sidepane.js
+++ src/scripts/components/Sidepane/Sidepane.js
@@ -1,13 +1,31 @@
-import { h } from 'preact';
+import { h, Fragment } from 'preact';
+import { useContext } from 'preact/hooks';
+import { ConnectionContext } from '../ConnectionContext';
 import Section from '../Section/Section';
 import OrderedContextsList from '../OrderedContextsList/OrderedContextsList';
+import SVG from '../SVG';
 
 import styles from "./Sidepane.scss";
+import WarningIcon from '../../../icons/warning.svg';
 
 const Sidepane = ({context, ...props}) => {
+    
+    let { shouldUpdate } = useContext(ConnectionContext)
+
+
     return (
         <div className={styles.sidepane} {...props}>
             <Section title="Info about the context" id="context-info">
+                {
+                    shouldUpdate && (
+                        <>
+                            <div>
+                                <SVG src={WarningIcon} className="inline-icon" />
+                                <span style={{marginLeft: '4px', color: 'hsl(57deg 100% 36%)'}}>There were some changes in the page. The stacking context informations might be out of date.</span>
+                            </div>
+                        </>
+                    )
+                }
                 {
                     context && (context.passedChecks?.length > 0) && (
                         <ul>
